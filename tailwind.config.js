9b6766c157d9bd7ad540473989e022cc34dac686
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-01-23 12:17:50 +0800

    Update content paths in tailwind.config.js
    Add public/index.html to Tailwind CSS content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // 이 경로가 정확해야 스타일이 생성됩니다.
+    "./public/index.html"
   ],
   theme: {
     extend: {},
